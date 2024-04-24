fe8fdc4d4ccf4313f8a62f8cb0b1ea3da51066fe
cherniuta <kukushonok2222@gmail.com>
2024-04-24 21:40:49 +0300

    add storage

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"golang.org/x/exp/slog"
+	"os"
 	"rest_api_project/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Print(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	storage, err := 
+}
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+
+	}
+
+	return log
 }
